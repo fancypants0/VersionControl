366e0bee88886d32639ae309362472119b3cdd40
Bálint Tóth <nagyonsoktejberizs@gmail.com>
2021-10-21 22:08:23 +0200

    Negyedik feladat

--- gyak06_jlv3dc/gyak06_jlv3dc/Form1.cs
+++ gyak06_jlv3dc/gyak06_jlv3dc/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,15 @@ namespace gyak06_jlv3dc
 {
     public partial class Form1 : Form
     {
+        List<Tick> ticks; 
+
         public Form1()
         {
             InitializeComponent();
+            PortfolioEntities context = new PortfolioEntities();
+            context.Ticks.Load();
+            ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = ticks;
         }
     }
 }
