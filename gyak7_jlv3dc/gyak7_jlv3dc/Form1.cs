49fc8e556f74aa8a0285e8e4bbd15a9077f8332a
Bálint Tóth <nagyonsoktejberizs@gmail.com>
2021-10-25 01:27:23 +0200

    hármas feladat

---
+++ gyak7_jlv3dc/gyak7_jlv3dc/Form1.cs
@@ -0,0 +1,35 @@
+﻿using gyak7_jlv3dc.mnb;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyak7_jlv3dc
+{
+    public partial class Form1 : Form
+    {
+        MNBArfolyamServiceSoapClient mnb_service = new MNBArfolyamServiceSoapClient();
+        GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+
+        public Form1()
+        {
+            InitializeComponent();
+            get_rates();
+        }
+
+        void get_rates()
+        {
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+
+            var response = mnb_service.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+        }
+    }
+}
---
+++ gyak7_jlv3dc/gyak7_jlv3dc/rate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace gyak7_jlv3dc
+{
+    class rate
+    {
+        public DateTime date { get; set; }
+        public string currency { get; set; }
+        public int value { get; set; }
+    }
+}
