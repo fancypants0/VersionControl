a291d424f1450c87b97e404b1f3e41d2ca1f2834
Bálint Tóth <nagyonsoktejberizs@gmail.com>
2021-12-11 00:07:03 +0100

    4

--- jlv3dc_gyak11/jlv3dc_gyak11/Form1.cs
+++ jlv3dc_gyak11/jlv3dc_gyak11/Form1.cs
@@ -45,6 +45,32 @@ namespace jlv3dc_gyak11
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
         }
 
         void fv()
